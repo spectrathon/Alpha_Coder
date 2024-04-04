123538b046e92b976ed3e45bf32ef92d5c6aabf6
yadavsk098 <yadavsk098@gmail.com>
2024-04-04 11:12:12 +0530

    uploading reamaining file

---
+++ src/app/Quiz/page.jsx
@@ -0,0 +1,14 @@
+import Quiz from "@/components/Quiz/Quiz";
+
+
+const page = () => {
+    return (
+        <div>
+            <h1>welcome to quiz secttion </h1>
+            <Quiz />
+        </div>
+    );
+}
+
+export default page;
+
